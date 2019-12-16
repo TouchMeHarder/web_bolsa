45727de8c756ef4714597246aefff5787a7b2668
Paul <paulur2@gmail.com>
2019-12-16 13:00:15 +0100

    noon commit

---
+++ src/Controller/Admin/UserController.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\EasyAdminController as BaseAdminController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
+class UserController extends BaseAdminController
+{
+
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    /**
+     * UserController constructor.
+     *
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     */
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function persistEntity($entity)
+    {
+        $this->encodePassword($entity);
+        parent::persistEntity($entity);
+    }
+
+    public function updateEntity($entity)
+    {
+        $this->encodePassword($entity);
+        parent::updateEntity($entity);
+    }
+
+    public function encodePassword($user)
+    {
+        if (!$user instanceof User) {
+            return;
+        }
+
+        $user->setPassword(
+            $this->passwordEncoder->encodePassword($user, $user->getPassword())
+        );
+    }
+
+}
--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -95,7 +95,8 @@ class LoginAuthenticator extends AbstractFormLoginAuthenticator implements Passw
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('easyadmin'));
     }
 
     protected function getLoginUrl()
